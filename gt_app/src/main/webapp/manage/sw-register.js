719497c40694bdd17143ba3024c303b796fde8cc
dj <1193933@qq.com>
2017-12-05 18:03:43 +0800

    打包

--- gt_app/src/main/webapp/manage/sw-register.js
+++ gt_app/src/main/webapp/manage/sw-register.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker&&navigator.serviceWorker.register('/service-worker.js?v=20171205174819').then(function(){navigator.serviceWorker.addEventListener('message',function(e){if(e.data==='sw.update'){setTimeout(function(){return location.reload()},16)}})});
+navigator.serviceWorker&&navigator.serviceWorker.register('/service-worker.js?v=20171205180254').then(function(){navigator.serviceWorker.addEventListener('message',function(e){if(e.data==='sw.update'){setTimeout(function(){return location.reload()},16)}})});
